ba73046e9d27deb5abb85efa69e4958233efb8c7
BuiPhiThong <thongbphe171683@fpt.edu.vn>
2024-10-04 17:04:11 +0700

    #2: Db conneect , create database , table

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 require('dotenv').config();
+const dbConnect = require('./config/dbconnect')
+const initRoutes = require('./routes/index')
+
 
 const app = express();
 
@@ -7,10 +10,9 @@ const port = process.env.PORT || 8888;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+dbConnect()
 
-app.use('/', (req, res) => {
-    res.send('Server on!!');
-});
+initRoutes(app)
 
 app.listen(port, () => {
     console.log('Server running on the port ' + port);
